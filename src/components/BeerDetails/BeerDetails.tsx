aef091f1bbba311cb3f4d928d2b2e791ecaed996
pfmnowak <58272232+pfmnowak@users.noreply.github.com>
2023-04-03 11:58:05 +0200

    Style the BeerDetails component

--- src/components/BeerDetails/BeerDetails.tsx
+++ src/components/BeerDetails/BeerDetails.tsx
@@ -12,27 +12,38 @@ const BeerDetails = ({ beerItem }: BeerDetailsProps) => {
 
 	return (
 		<div className={classes.beer}>
-			<div>
-				<IconButton onClick={() => navigate(-1)} name='icon-chevron-left1' />
-			</div>
-			<div className={classes['beer__img-container']}>
-				<img src={beerItem.imageUrl} className={classes['beer__img']} />
+			<div className={classes['beer__header']}>
+				<div className={classes['beer__icon']}>
+					<IconButton onClick={() => navigate(-1)} name='icon-chevron-left1' />
+				</div>
+				<div className={classes['beer__img-container']}>
+					<img src={beerItem.imageUrl} className={classes['beer__img']} />
+				</div>
+
+				<div className={classes['beer__main-info']}>
+					<p className={classes['beer__name']} title={beerItem.name}>
+						{beerItem.name}
+					</p>
+					<p className={classes['beer__tagline']} title={beerItem.tagline}>
+						{beerItem.tagline}
+					</p>
+
+					<div className={classes['beer__parameters']}>
+						<div className={classes['beer__abv']}>
+							<p className={classes['beer__abv-label']}>abv</p>
+							<p className={classes['beer__abv-value']}>{beerItem.abv ? beerItem.abv : '-'}</p>
+						</div>
+						<div className={classes['beer__ibu']}>
+							<p className={classes['beer__ibu-label']}>ibu</p>
+							<p className={classes['beer__ibu-value']}>{beerItem.ibu ? beerItem.ibu : '-'}</p>
+						</div>
+					</div>
+				</div>
 			</div>
 			<div className={classes['beer__info']}>
-				<p className={classes['beer__name']} title={beerItem.name}>
-					{beerItem.name}
-				</p>
-				<p className={classes['beer__id']}>#{beerItem.id}</p>
-				<p className={classes['beer__tagline']} title={beerItem.tagline}>
-					{beerItem.tagline}
-				</p>
 				<p className={classes['beer__description']} title={beerItem.description}>
 					{beerItem.description}
 				</p>
-				<p className={classes['beer__abv-label']}>abv</p>
-				<p className={classes['beer__abv']}>{beerItem.abv}</p>
-				<p className={classes['beer__ibu-label']}>ibu</p>
-				<p className={classes['beer__ibu']}>{beerItem.ibu}</p>
 				{/* ingredients */}
 			</div>
 		</div>
